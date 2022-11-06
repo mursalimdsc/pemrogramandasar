3bdff18a69ce45f2c00701fd2eef1e347b752691
mursalimdsc <mursalim.dsc@gmail.com>
2022-11-06 19:42:50 +0700

    Menambahkan latihan 3 Operasi Matematika

---
+++ latihan3_operasi.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 19:42:00 2022
+
+@author: salim
+"""
+# Variabel
+var_a=2
+
+nilai1 =2
+nilai2 =6
+nilai3 =10
+nilai4 =9
+nilai5 =10
+
+# Operasi
+nilai1 += var_a   
+nilai2 *= var_a
+nilai3 /= var_a
+nilai4 %= var_a
+nilai5 **= var_a
+
+print(nilai1)
+print(nilai2)
+print(nilai3)
+print(nilai4)
+print(nilai5)
