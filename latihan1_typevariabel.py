29c536a2dcdefd657464a3ecb10a11d6733d4922
mursalimdsc <mursalim.dsc@gmail.com>
2022-11-06 19:18:09 +0700

    Menambahkan file baru latihan1 dan spyproject

---
+++ latihan1_typevariabel.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 18:55:39 2022
+
+@author: salim
+"""
+# Penentuan Tipe Variabel
+a =10  
+b ="Hi Python Informatika UMKABA"  
+c = 10.5  
+print(type(a))  
+print(type(b))  
+print(type(c))  
+
