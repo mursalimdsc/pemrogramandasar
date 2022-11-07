bc91cfd6092fe44655ad72e1dbf5a0a1262c8524
mursalimdsc <mursalim.dsc@gmail.com>
2022-11-07 13:06:40 +0700

    Merubah latihan 4 kondisi

--- latihan4_kondisi.py
+++ latihan4_kondisi.py
@@ -4,6 +4,10 @@ Created on Mon Nov  7 11:20:26 2022
 
 @author: salim
 """
+#inputan varibel
+
+nama  = input("Masukkan Nama Lengkap: ")
+prodi = input("Masukkan Program Studi: ")
 
 nilai = int(input("Masukkan Nilai: "))
 
@@ -11,3 +15,11 @@ if(nilai) >=90:
     print ("Grade A")
 else:
     print("Grade B")
+    
+def pekalian(a,b):
+    nilai_a = a
+    nilai_b = b
+    
+    hitung_perkalian = nilai_a*nilai_b
+    
+    return hitung_perkalian
