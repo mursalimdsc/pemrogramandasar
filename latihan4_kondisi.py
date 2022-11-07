2cf07592a0743fcac3e539e00a4c7d8f2195bd79
mursalimdsc <mursalim.dsc@gmail.com>
2022-11-07 11:21:58 +0700

    Menambahkan file latihan 4 kondisi

---
+++ latihan4_kondisi.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 11:20:26 2022
+
+@author: salim
+"""
+
+nilai = int(input("Masukkan Nilai: "))
+
+if(nilai) >=90:
+    print ("Grade A")
+else:
+    print("Grade B")
