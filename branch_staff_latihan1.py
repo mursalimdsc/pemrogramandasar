a51d92de4b72ca32df68a52bacf4a428541dd2e6
mursalimdsc <mursalim.dsc@gmail.com>
2022-11-07 11:59:57 +0700

    Megubah branch staff latihan1 dan menambahkan string

--- branch_staff_latihan1.py
+++ branch_staff_latihan1.py
@@ -5,5 +5,7 @@ Created on Mon Nov  7 11:55:19 2022
 @author: salim
 """
 str = "Mursalim adalah staff pengajar di Universitas Muhammadiyah Kendal Batang"
+str_2 = "Universitas Muhammadiyah Kendal Batang"
 print(len(str))
 print(str[21:30])
+
